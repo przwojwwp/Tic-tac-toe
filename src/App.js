e3759b20909a45badb397f30c48ebbd8d15e2de3
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-08 13:55:52 +0200

    feat: add X or O only for empty squares

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { useState } from 'react';
 
 function Square({ value, onSquareClick })
 {
-  return <button className="square" onClick={onSquareClick}>
-    {value}
-  </button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board()
 {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i)
   {
+    if (squares[i])
+    {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext)
+    {
+      nextSquares[i] = "X";
+    } else
+    {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
