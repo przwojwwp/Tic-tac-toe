0a968d476688ba65ddaee747c59e15344ccd93ab
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-09 19:08:26 +0200

    refactor: Board component to dynamically generate squares using loops

--- src/App.js
+++ src/App.js
@@ -40,24 +40,27 @@ function Board({ xIsNext, squares, onPlay })
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const boardRows = [];
+  const boardSize = 3;
+  for (let row = 0; row < boardSize; row++)
+  {
+    let squaresInRow = [];
+    for (let col = 0; col < boardSize; col++)
+    {
+      const squareIndex = row * boardSize + col;
+      squaresInRow.push(<Square
+        key={squareIndex}
+        value={squares[squareIndex]}
+        onSquareClick={() => handleClick(squareIndex)}
+      />)
+    }
+    boardRows.push(<div key={row} className="board-row">{squaresInRow}</div>)
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardRows}
     </>
   );
 }
