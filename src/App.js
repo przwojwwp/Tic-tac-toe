490670a89fd139967651b89c651fb1c320c4535c
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-10 10:07:11 +0200

    feat: add button to sort moves in ascending/descending order

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ export default function Game()
 {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [isAscending, setIsAscending] = useState(true);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -82,6 +83,11 @@ export default function Game()
   function jumpTo(nextMove)
   {
     setCurrentMove(nextMove);
+  }
+
+  function toggleSortOrder()
+  {
+    setIsAscending(!isAscending);
   }
 
   const moves = history.map((squares, move) =>
@@ -108,13 +114,18 @@ export default function Game()
     )
   })
 
+  const sortedMoves = isAscending ? moves : [...moves].reverse();
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <button onClick={toggleSortOrder}>
+          {isAscending ? 'Sort descending' : 'Sort ascending'}
+        </button>
+        <ol>{sortedMoves}</ol>
       </div>
     </div>
   )
